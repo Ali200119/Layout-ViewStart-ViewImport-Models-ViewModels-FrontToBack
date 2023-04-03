ce2d53ae7ed75043e284f216d7244f0dd9ec2e08
Ali200119 <aliit@code.edu.az>
2023-04-04 02:32:17 +0400

    Update

---
+++ Practice/Practice/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewData["names"] = new string[] { "Elcan", "Pervin", "Aqshin", "Ali"};
+
+            //ViewBag.numbers = new int[] { 10, 20, 30, 7, 87, 23 };
+
+            //List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
+
+            //ViewBag.name = "Elcan";
+
+            TempData["surname"] = "Qurbanov";
+
+            //return View();
+
+            return RedirectToAction(nameof(Detail));
+        }
+
+        public IActionResult Detail()
+        {
+
+
+            return View();
+        }
+    }
+}
--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,7 +1,11 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
